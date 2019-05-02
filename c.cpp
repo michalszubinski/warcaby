5eab6dfea406815e959404fa89904944b50bd281
michalszubinski <43965685+michalszubinski@users.noreply.github.com>
2019-05-02 19:44:51 +0200

    Update c.cpp

--- c.cpp
+++ c.cpp
@@ -16,3 +16,10 @@ bool c::operator!=(c other)
     if((((this -> x)==(other.x))&&((this -> y)==(other.y)))) return 0;
     else return 1;
 }
+
+bool c::good()
+{
+    if(((this -> x)>0)&&((this -> x)<8)&&((this -> y)>0)&&((this -> y)<8)) return 1;
+
+    return 0;
+}
