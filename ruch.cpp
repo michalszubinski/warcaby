948795aa353427b0dbfe4a3a25ad9ce94fada6b7
michalszubinski <43965685+michalszubinski@users.noreply.github.com>
2019-05-07 12:48:24 +0200

    Update ruch.cpp

--- ruch.cpp
+++ ruch.cpp
@@ -16,3 +16,8 @@ bool ruch::operator!=(ruch other)
     if((((this -> o)==(other.o))&&((this -> n)==(other.n))&&((this -> wartoscruchu)==(other.wartoscruchu))&&((this -> id)==(other.id))&&((this -> bicie)==(other.bicie))&&((this -> bicieid)==(other.bicieid)))) return 0;
     else return 1;
 }
+
+void ruch::show()
+{
+    printf("ID: %d, OLD = (%d,%d), NEW = (%d,%d), BICIE = %d, BICIEID = %d, TEAM = %d, WARTOSC = %d\n",id,x,y,x,y,bicie,bicieid,team,wartoscruchu);
+}
